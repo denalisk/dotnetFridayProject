6b1d5d8af2ed70c273b55a5cc861d372c87f5643
Sam Kirsch <samdkirsch2@gmail.com>
2017-05-12 15:01:06 -0400

    determine proper url for ajax search call

--- portfolio/wwwroot/js/scripts.js
+++ portfolio/wwwroot/js/scripts.js
@@ -1,3 +1,27 @@
+var sortStars = function(resultsArray) {
+  for (let i = 0; i < resultsArray.length; i++) {
+    
+  }
+}
+
 ﻿$(document).ready(function () {
     console.log("js loaded!");
+    $("#github-ajax").click(function() {
+      console.log("clicky");
+      $.ajax({
+        url: 'https://api.github.com/search/repositories?q=user%3Adenalisk+stars%3A>0&type=Repositories',
+        type: 'GET',
+        datatype: 'json',
+        success: function(result) {
+          for (var item in result) {
+            ;
+          }
+          console.log("success");
+          console.log(result);
+        },
+        error: function(result) {
+          console.log("error");
+        }
+      })
+    });
 })
